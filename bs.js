41d09e10792d5a4ce2871e17ea285c22cf90d5b1
Kunalhats <109820362+Kunalhats@users.noreply.github.com>
2024-01-31 08:03:35 +0000

    major changes

---
+++ bs.js
@@ -0,0 +1,8 @@
+let userName="kunal";
+const userId=23223;
+userCity="Dhanbad";
+
+userName="Badu"
+userCity="Siwan"
+console.table([userName,userId,userCity])
+console.log(userName,userId,userCity)
