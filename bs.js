3f167f761f3bd4c86eba4ecea55e41518a9d4c84
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-02-02 07:54:32 +0000

    New Changes

--- bs.js
+++ bs.js
@@ -1,8 +1,63 @@
+
+// Primitive data types
 let userName="kunal";
 const userId=23223;
 userCity="Dhanbad";
 
-userName="Badu"
-userCity="Siwan"
-console.table([userName,userId,userCity])
-console.log(userName,userId,userCity)
+let salary=true;
+let GST=null;
+
+// console.log(typeof userName)
+//console.log(typeof userId)
+// console.log(typeof salary)
+//console.log(typeof GST)
+
+/*console.log(typeof 23223)
+console.log(typeof "kunal")*/
+
+/*convertion operations
+
+let result1=Number(userName)
+console.log(result1)
+let result=Number(userCity)
+console.log(result)
+
+let result2=String(userId)
+console.log(result2)
+
+console.log(typeof result)
+console.log(typeof result1)
+console.log(typeof result2)
+*/
+
+//non-primitive data types
+
+/*
+let EMP_1=["kunal",23223, "Dhanbad", "5Lakh"]
+console.log(typeof EMP_1)
+
+let EMP1={
+    name:"kunal",
+    ID:23223,
+    City:"Dhanbad",
+    Salary:"5Lakh"
+}
+console.log(typeof EMP1)
+
+const kunal=function(){
+    console.log("Best Developer")
+}
+
+console.log(kunal)*/
+
+// Operations(+-*/%)
+
+/*
+let a=10;
+let b=15;
+console.log((a+2)+b)
+console.log((a-2)+b)
+console.log((a*2)-b)
+console.log((a/2)*b)
+console.log((a%2)+b)
+*/
