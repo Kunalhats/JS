0352a94353fa58c5e3ce79044c97b9bc1071a5fd
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-02-06 10:37:37 +0000

    new added

---
+++ number.js
@@ -0,0 +1,32 @@
+let x=1265.456;
+let y=223;
+
+// toExponential() returns a string, with a number rounded and written using exponential notation.
+//console.log(x.toExponential(2));
+
+/*  toFixed() Method  returns a string, with the number written with a specified number of decimals:
+console.log(x.toFixed(4)); */
+
+/* toPrecision() returns a string, with a number written with a specified length: 
+console.log(x.toPrecision(4)); */
+
+/* valueOf() returns a number as a number.
+console.log(x.valueOf()); */
+
+
+//console.log(x.toLocaleString('en-In'));
+
+//console.log(Math.abs(x));
+//console.log(Math.round(x));
+
+/* To control the logic of rounding off function we use ceiling and floor functions
+
+console.log(Math.ceil(x));
+console.log(Math.floor(x)); */
+
+/*To find the minimum and maximum value from an array of data we generally use min and max method
+
+console.log(Math.min(y,x));
+console.log(Math.max(y,x)); */
+
+
---
+++ string.js
@@ -0,0 +1,58 @@
+const string1=new String("kunal love csk");
+const string2=new String(" "+"and India");
+
+/* String concat(str) Method
+console.log(string1.concat(string2)); */ //console.log(string1+string2))
+
+/*String Interpolation Method
+console.log(`tell me something ${string1} more${string2}` )*/
+
+/*String charAt(index) Method
+console.log(string1.charAt(7));*/
+
+//UTF-16 code unit value of the character at the given index. 
+//console.log(string1.charCodeAt(7))
+
+/*String indexOf(str) Method
+console.log(string1.indexOf("c"));*/
+
+//String lastIndexOf(str) Method
+//console.log(string1.lastIndexOf("k"));
+
+//String toLowerCase() and toUpperCase() Method
+/*console.log(string1.toUpperCase());
+console.log(string2.toLowerCase());*/
+
+//JavaScript String slice(beginIndex, endIndex) Method
+//console.log(string1.slice(2,8));
+
+//JavaScript String trim() Method
+//console.log(string1.trim());
+
+
+/*Returns true if the string ends with the given string, otherwise false. 
+console.log(string1.endsWith("csk"))*/
+
+/*Returns true if the string contains the given string, otherwise false. 
+console.log(string1.includes("love")); */
+
+
+/*Returns a new string with some content padded to the end and start of the string.
+console.log(string1.padEnd(50, "world"))
+console.log(string1.padStart(50, "world"));*/
+
+/* Returns a new string which contains the specified number of copies of the string. 
+console.log(string2.repeat(4)); */
+
+/* Returns a new string with some or all matches of a regular expression replaced by a replacement string. 
+
+console.log(string1.replaceAll("love","like")); */
+
+/*Returns the index within the string of the first occurrence of the specified value, or -1 if not found. 
+console.log(string1.search("love")); */
+
+/* Returns an array of strings split at the given index. 
+console.log(string1.split(" ")); */
+
+/*Returns the string representation of the specified object. 
+console.log(string1.toString()); */
