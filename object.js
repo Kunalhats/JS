77bb3ae89ace4aa2982727dd1cf72b067b270d6e
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-02-09 09:01:24 +0000

    new added

--- object.js
+++ object.js
@@ -2,15 +2,9 @@ let Emp1={
     name:"kunal",
     id:2124,
     designation:"developer",
-    location:"kolkata",
-    salary:500000,
-    company:"deloitte"
 }
 
 let Emp2={
-    name:"amit",
-    id:2125,
-    designation:"developer",
     location:"kolkata",
     salary:500000,
     company:"deloitte"
@@ -21,9 +15,40 @@ let Emp2={
 //console.log(Emp1.name);
 //console.log(Emp2.id);
 
-//const value=Object.assign(Emp2,Emp1)
+//const value=Object.assign( Emp2,Emp1);
 //console.log(value);
 
 //spread opearator
-let value={...Emp2, ...Emp1};
-console.log(value);
+//let value={...Emp2, ...Emp1};
+//console.log(value);
+
+//How to make objects using constructor
+ /*const owner=new Object() //This is called a singleton object\
+
+ owner.id=2323;
+ owner.name="kunal";
+ owner.salary=500000;
+ console.log(owner); */
+
+ //Working with Objects/arrays inside an Object.
+
+ const owner = {
+    name: "kunal",
+    city:"dhanbad",
+    id:{
+        salary: 4400000,
+        design: "developer"
+    },
+    company:"google"
+ }
+ //console.log(owner.id.design);
+
+ //console.log(Object.keys(owner));
+ //console.log(Object.values(owner));
+
+
+ // Object destructure
+
+const {design: name}=owner;
+console.log(name);
+
