faa851d0e142b5c3cc407bf27ba9bc3a64825409
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-02-16 07:51:44 +0000

    changes

---
+++ arrowfunction.js
@@ -0,0 +1,15 @@
+//explicit return method in an arrow function
+/*const mul=(num1)=>{
+    return num1*5;
+}
+console.log(mul(5));  */
+
+
+//implicit return because we are not mentioning return statement here and JS considers it to be implied automatically.
+//Arrow Function without Parameters
+//const name=() => ("kunal")
+//console.log(name());
+
+//Arrow Function with Parameters
+//const add=(num1,num2)=>(num1+num2)
+//console.log(add(2,3))
---
+++ this.js
@@ -0,0 +1,19 @@
+/*this can be used with object
+const details={
+    user:"kunal",
+    pass:"mahi",
+    mail:"ks12@gmail.com",
+
+    result:function(){
+    console.log(`${this.user}, welcome to website`);
+}
+}
+details.result()
+*/
+/*this cannot be used with function
+function name(){
+    const username="kunal"
+    console.log(this.name)
+}
+name()
+*/
