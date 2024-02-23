8b233d2fd725b060aebc9cd90addf9fbb4669a8f
Kunal Kumar Singh <109820362+Kunalhats@users.noreply.github.com>
2024-02-23 11:02:27 +0000

    Assignment Completed

---
+++ Assignment.js
@@ -0,0 +1,114 @@
+const books = [
+    {
+    "title": "To Kill a Mockingbird",
+    "author": "Harper Lee",
+    "genre": "Fiction",
+    "year": 1960,
+    "publisher": "J. B. Lippincott & Co.",
+    "pages": 281
+    },
+    {
+    "title": "1984",
+    "author": "George Orwell",
+    "genre": "Dystopian Fiction",
+    "year": 1949,
+    "publisher": "Secker & Warburg",
+    "pages": 328
+    },
+    {
+    "title": "The Great Gatsby",
+    "author": "F. Scott Fitzgerald",
+    "genre": "Fiction",
+    "year": 1925,
+    "publisher": "Charles Scribner's Sons",
+    "pages": 180
+    },
+    {
+    "title": "Pride and Prejudice",
+    "author": "Jane Austen",
+    "genre": "Romance",
+    "year": 1813,
+    "publisher": "T. Egerton, Whitehall",
+    "pages": 279
+    },
+    {
+    "title": "The Catcher in the Rye",
+    "author": "J.D. Salinger",
+    "genre": "Fiction",
+    "year": 1951,
+    "publisher": "Little, Brown and Company",
+    "pages": 277
+    },
+    {
+    "title": "Harry Potter and the Philosopher's Stone",
+    "author": "J.K. Rowling",
+    "genre": "Fantasy",
+    "year": 1997,
+    "publisher": "Bloomsbury",
+    "pages": 223
+    },
+    {
+    "title": "The Hobbit",
+    "author": "J.R.R. Tolkien",
+    "genre": "Fantasy",
+    "year": 1937,
+    "publisher": "George Allen & Unwin",
+    "pages": 310
+    },
+    {
+    "title": "Moby-Dick; or, The Whale",
+    "author": "Herman Melville",
+    "genre": "Adventure",
+    "year": 1851,
+    "publisher": "Harper & Brothers",
+    "pages": 206
+    },
+    {
+    "title": "The Lord of the Rings",
+    "author": "J.R.R. Tolkien",
+    "genre": "Fantasy",
+    "year": 1954,
+    "publisher": "Allen & Unwin",
+    "pages": 1009
+    },
+    {
+    "title": "The Hunger Games",
+    "author": "Suzanne Collins",
+    "genre": "Dystopian Fiction",
+    "year": 2008,
+    "publisher": "Scholastic Press",
+    "pages": 374
+    }
+]
+
+// Log only those books which are from the genre of "Fantasy".
+/*
+books.forEach((value)=>{
+    if(value.genre=='Fantasy'){
+    console.log(value)
+    }
+}
+)
+*/
+
+// Log only those books which are printed after 1900 and are in the genre of "Adventure".
+/*
+books.forEach((value)=>{
+    if(value.year>1990 && value.genre=='Adventure'){
+    console.log(value)
+    }
+    else{
+        console.log("Not Found")
+    }
+}
+)
+*/
+// Log only those books which have more than 300 pages, are printed after 1900 and in the genre of "Dystopian Fiction"
+/*
+books.forEach((value)=>{
+    if(value.pages>300 && value.year>1990 && value.genre=='Dystopian Fiction'){
+    console.log(value)
+    }
+}
+)
+*/
